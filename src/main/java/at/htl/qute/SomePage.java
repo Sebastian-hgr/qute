c013d1003cc3f9f4734af3103e395abd69bc65e8
Sebastian-hgr <sebihager5@gmail.com>
2022-11-28 14:57:58 +0100

    safe template

--- src/main/java/at/htl/qute/SomePage.java
+++ src/main/java/at/htl/qute/SomePage.java
@@ -1,4 +1,5 @@
 package at.htl.qute;
+import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 
@@ -13,16 +14,15 @@ import static java.util.Objects.requireNonNull;
 @Path("/some-page")
 public class SomePage {
 
-    private final Template page;
-
-    public SomePage(Template page) {
-        this.page = requireNonNull(page, "page is required");
+    @CheckedTemplate
+    public static class Templates {
+        public static native TemplateInstance page(String name);
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance get(@QueryParam("name") String name) {
-        return page.data("name", name);
+        return Templates.page(name);
     }
 
 }
